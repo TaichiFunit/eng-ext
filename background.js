5005a50cfb919aeeaba29d17e9c5ece77b24a380
BuildTools <unconfigured@null.spigotmc.org>
2025-02-12 02:07:22 +0900

    complete

--- background.js
+++ background.js
@@ -1,32 +1,29 @@
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "trigger_download") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const activeTab = tabs[0];
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: activeTab.id },
-          function: getDownloadUrl,
-        },
-        (results) => {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError);
-            return;
-          }
-          if (results && results[0] && results[0].result) {
-            const url = results[0].result;
-            chrome.downloads.download({ url: url }, (downloadId) => {
-              if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError);
-              } else {
-                console.log(`Download started with ID: ${downloadId}`);
-              }
-            });
-          } else {
-            console.error("No URL found in the active tab.");
-          }
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url) {
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tabId },
+        function: getDownloadUrl,
+      },
+      (results) => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+          return;
         }
-      );
-    });
+        if (results && results[0] && results[0].result) {
+          const url = results[0].result;
+          chrome.downloads.download({ url: url }, (downloadId) => {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError);
+            } else {
+              console.log(`Download started with ID: ${downloadId}`);
+            }
+          });
+        } else {
+          console.error("No URL found in the active tab.");
+        }
+      }
+    );
   }
 });
 
@@ -35,11 +32,3 @@ function getDownloadUrl() {
   const button = document.querySelector("div.lecture-attachment button");
   return button ? button.getAttribute("data-href") : null;
 }
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.url) {
-    chrome.downloads.download({
-      url: request.url,
-    });
-  }
-});
